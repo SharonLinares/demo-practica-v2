f2a26008579e9d28a824a6866f7eb7884a31d71c
Sharon Linares <akiralyn02@gmail.com>
2025-06-10 14:51:38 +0200

    se hizo el punto 5 de la tarea

--- src/main/java/com/example/demo/DemoPracticaV2Application.java
+++ src/main/java/com/example/demo/DemoPracticaV2Application.java
@@ -3,11 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.dto.VentaDto;
+
 @SpringBootApplication
 public class DemoPracticaV2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoPracticaV2Application.class, args);
+		
+		VentaDto ventaDto = new VentaDto("camara", 2, 10, 20);
+		
+		
+		
 	}
 
 }
--- src/main/java/com/example/demo/controller/VentaController.java
+++ src/main/java/com/example/demo/controller/VentaController.java
@@ -9,13 +9,12 @@ public class VentaController {
 	
 	private List<VentaDto> ventas = new ArrayList<>();
 	
-	
 	public void crearVenta(VentaDto ventadto) {
 		this.ventas.add(ventadto);
 	}
 	
 	
-	// 	public VentaDto(String nombreProducto, int cantidad, double valorUnitario, double valorTotal) {
+
 	public void imprimirVentas() {
 		for (VentaDto ventaDto : ventas) {
 			System.out.println(ventaDto.getNombreProducto());
@@ -23,6 +22,22 @@ public class VentaController {
 			System.out.println(ventaDto.getValorUnitario());
 			System.out.println(ventaDto.getValorTotal());
 		}
+	}
+	
+	public static void main(String[] args) {
+		
+		VentaController ventaController = new VentaController();
+		
+		VentaDto v = new VentaDto("mause",2, 10, 20);
+		VentaDto v2 = new VentaDto("teclado",2, 15, 30);
+		VentaDto v3 = new VentaDto("monitor",2, 9, 18);
+		
+		
+		ventaController.crearVenta(v);
+		ventaController.crearVenta(v2);
+		ventaController.crearVenta(v3);
+		ventaController.imprimirVentas();
+		
 	}
 
 }
