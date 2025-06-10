103b31f77e7063ef19640d578de607af5edf39f9
Sharon Linares <akiralyn02@gmail.com>
2025-06-10 14:35:56 +0200

    cree el metodo imprimir ventas y imprimi sus atributos en consola

--- src/main/java/com/example/demo/controller/VentaController.java
+++ src/main/java/com/example/demo/controller/VentaController.java
@@ -1,5 +1,28 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demo.dto.VentaDto;
+
 public class VentaController {
+	
+	private List<VentaDto> ventas = new ArrayList<>();
+	
+	
+	public void crearVenta(VentaDto ventadto) {
+		this.ventas.add(ventadto);
+	}
+	
+	
+	// 	public VentaDto(String nombreProducto, int cantidad, double valorUnitario, double valorTotal) {
+	public void imprimirVentas() {
+		for (VentaDto ventaDto : ventas) {
+			System.out.println(ventaDto.getNombreProducto());
+			System.out.println(ventaDto.getCantidad());
+			System.out.println(ventaDto.getValorUnitario());
+			System.out.println(ventaDto.getValorTotal());
+		}
+	}
 
 }
--- src/main/java/com/example/demo/dto/VentaDto.java
+++ src/main/java/com/example/demo/dto/VentaDto.java
@@ -2,4 +2,49 @@ package com.example.demo.dto;
 
 public class VentaDto {
 
+	private String nombreProducto;
+	private int cantidad;
+	private double valorUnitario;
+	public double valorTotal;
+
+	public VentaDto(String nombreProducto, int cantidad, double valorUnitario, double valorTotal) {
+		super();
+		this.nombreProducto = nombreProducto;
+		this.cantidad = cantidad;
+		this.valorUnitario = valorUnitario;
+		this.valorTotal = valorTotal;
+	}
+
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
 }
