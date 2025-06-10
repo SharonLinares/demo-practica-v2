d8cb8bf0ddf05cb251c99464edacc9be88a6d858
Sharon Linares <akiralyn02@gmail.com>
2025-06-10 14:00:53 +0200

    primer commit

---
+++ src/main/java/com/example/demo/DemoPracticaV2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPracticaV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPracticaV2Application.class, args);
+	}
+
+}
